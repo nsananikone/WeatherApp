b7a0f7d9eafea68c935c20ae00a2791ce1a85959
Dax911 <haydenaylor@icloud.com>
2022-07-13 18:33:05 -0500

    all working

--- src/index.js
+++ src/index.js
@@ -2,12 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Navigation from './components/navigation';
+import Pokemon from './pokemon';
+import About from './about';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="*" element={<About />} />
+        <Route path="pokemon" element={<Pokemon />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
